638d4986642ebf3e6456a4843b9d79ff2a3ebe7c
drQ2 <105453519+drQ2@users.noreply.github.com>
2026-02-13 11:43:47 +1300

    Delete Utils directory

--- Utils/ItemNameToResource.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace ACTQoL.Utils
-{
-    internal class ItemNameToResource
-    {
-        public static Dictionary<string, string> ItemToResource = new() { { "barbedhook", "hook" }, { "oldworldwhorl", "whorl" }, { "heartkelpsprout", "heartkelp" }, { "bloodstarlimb", "bloodstar" }, { "stainlessrelic", "key" } };
-    }
-}
